8f91f9c270cb2cde2f06df16b700098b1499b0aa
rachid chakor <chakor.rachid.1@gmail.com>
2023-01-24 23:41:48 -0500

    created a dictionary from PyDictionary() lib

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from PyDictionary import PyDictionary
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
 
-def word (request):
-    search = request.GET.get('search')
-    return render(request, 'word.html')
+def word(request):
+    search = request.GET.get('search0')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonym = dictionary.synonym(search)
+    antonym = dictionary.antonym(search)
+    # if meaning.keys()=='Noun':
+    #     meaning=meaning['Noun']
+    # elif meaning.keys()=='Verb':
+    #     meaning = meaning['Verb']
+    context = {
+        'meaning': meaning,
+        'synonym': synonym,
+        'antonym': antonym
+    }
+    return render(request, 'word.html', context)
